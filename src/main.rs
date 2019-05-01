dd16fe186c37e89a30d016af6abd271a3d584be1
Alberto Méndez Hernández <albertomendezhdz@gmail.com>
2019-05-01 21:06:15 +0200

    Application created with '/app' root path

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
+use actix_web::{App, HttpRequest, http::Method};
 
-fn index(_req: &HttpRequest) -> &'static str {
-    "Hello world!"
+fn make_app() {
+    fn index(_req: &HttpRequest) -> &'static str {
+        "Hello world!"
+    }
+
+    let _app = App::new()
+                .prefix("/app")
+                .resource("/index.html", |r| r.method(Method::GET).f(index))
+                .finish();
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8088")
-        .unwrap()
-        .run();
+    make_app();
+    // server::new(|| App::new().resource("/", |r| r.f(index)))
+    //     .bind("127.0.0.1:8088")
+    //     .unwrap()
+    //     .run();
 }
+
